4d0e34bc38ee3e4437766fc5aa27e37a61b7c78f
Olena Kuchko <kuchkoel@gmail.com>
2017-08-14 11:24:55 -0700

    practice with ajax

---
+++ src/AjaxDemo/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AjaxDemo.Models;
+
+namespace AjaxDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult HelloAjax() => Content("Hello from the controller", "text/plain");
+
+        public IActionResult Sum(int firstNumber, int secondNumber) => Content((firstNumber + secondNumber).ToString(), "text/plain");
+
+        public IActionResult DisplayObject()
+        {
+            Destination destination = new Destination("Tokyo", "Japan", 1);
+            return Json(destination);
+        }
+
+        public IActionResult DisplayViewWithAjax() => View();
+    }
+}
